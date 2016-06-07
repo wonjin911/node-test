ea07241ff18e6ee643db411379db8b83399cf558
golee <golee@ebay.com>
2016-06-07 17:31:37 +0900

    Added javascript event binding

---
+++ public/javascripts/addressbook.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	$('#city').on('keyup', function () {
+		// do something
+		console.log($('#city').val());
+	});
+});
--- routes/addressbook.js
+++ routes/addressbook.js
@@ -8,4 +8,17 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function (req, res) {
+	var name = req.body.name;
+	var zipcode = req.body.zipcode;
+	var address = req.body.address;
+
+	if (name && zipcode && address) {
+		res.send('Done. name=' + name + ', zipcode=' + zipcode + ', address=' + address);
+	}
+	else {
+		res.send('Incomplete. name=' + name + ', zipcode=' + zipcode + ', address=' + address);
+	}
+});
+
 module.exports = router;
