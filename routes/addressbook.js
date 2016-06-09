24607b7d599517cb0896622d489f17bf64d96261
golee <golee@ebay.com>
2016-06-09 17:35:34 +0900

    Added escrow service

--- routes/addressbook.js
+++ routes/addressbook.js
@@ -5,6 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 var Search = require('../lib/search');
+var Escrow = require('../lib/escrow');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,19 +21,22 @@ router.get('/zipcode', function (req, res, next) {
 		var formatted = addr.map(function (x) {
 			return x.ZIP_CD + '	' + x.ROAD_NAME_ADDRESS;
 		}).sort();
-		//console.log(formatted);
-
 		res.send(formatted);
 	});
 });
 
 router.post('/', function (req, res) {
+	var id = 'johndoe';
 	var name = req.body.name;
 	var zipcode = req.body.zipcode;
 	var address = req.body.address;
 
 	if (name && zipcode && address) {
-		res.send('Done. name=' + name + ', zipcode=' + zipcode + ', address=' + address);
+		zipcode = zipcode.split('\t');
+
+		Escrow.putAddress(id, name, zipcode[0], roadAddr[1], address, function (res) {
+			res.send('Done. name=' + name + ', zipcode=' + zipcode + ', address=' + address);
+		});
 	}
 	else {
 		res.send('Incomplete. name=' + name + ', zipcode=' + zipcode + ', address=' + address);
